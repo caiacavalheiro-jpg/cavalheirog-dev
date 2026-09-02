538a86e11d7b3e53d9dc6d371a58a12fca3c01c4
CAIÃ GOULART CAVALHEIRO <caia_cavalheiro@estudante.sesisenai.org.br>
2026-09-02 17:11:18 -0300

    teste

---
+++ projetoAlbum/script.js
@@ -0,0 +1,49 @@
+const cards1 = document.querySelector(".cards1");
+
+cards1.addEventListener("click", function(){
+    cards1.classList.toggle("selecionado")
+});
+
+const cards2 = document.querySelector(".cards2");
+
+cards2.addEventListener("click", function(){
+    cards2.classList.toggle("selecionado")
+});
+
+const cards3 = document.querySelector(".cards3");
+
+cards3.addEventListener("click", function(){
+    cards3.classList.toggle("selecionado")
+});
+
+const cards4 = document.querySelector(".cards4");
+
+cards4.addEventListener("click", function(){
+    cards4.classList.toggle("selecionado")
+});
+
+const cards5 = document.querySelector(".cards5");
+const cards6 = document.querySelector(".cards6");
+const cards7 = document.querySelector(".cards7");
+const cards8 = document.querySelector(".cards8");
+const cards9 = document.querySelector(".cards9");
+
+cards5.addEventListener("click", function(){
+    cards5.classList.toggle("selecionado")
+});
+
+cards6.addEventListener("click", function(){
+    cards6.classList.toggle("selecionado")
+});
+
+cards7.addEventListener("click", function(){
+    cards7.classList.toggle("selecionado")
+});
+
+cards8.addEventListener("click", function(){
+    cards8.classList.toggle("selecionado")
+});
+
+cards9.addEventListener("click", function(){
+    cards9.classList.toggle("selecionado")
+});
